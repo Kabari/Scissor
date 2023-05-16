6d0893341d000b0d46c8e47992ff4c916931745d
Emmanuel Kabari <kabariirenaeus@gmail.com>
2023-05-17 00:31:43 +0100

    created user table

---
+++ api/models/user.py
@@ -0,0 +1,35 @@
+from ..utils import db
+from datetime import datetime
+from uuid import uuid4
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(36), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.Text(100), nullable=False)
+    is_verified = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # def __init__(self, username, password):
+    #     # self.id = str(uuid4())
+    #     # self.uuid = str(uuid4())
+    #     self.username = username
+    #     self.password = password
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+
+    def save(self):
+        self.uuid = str(uuid4())
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
