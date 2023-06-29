0d67a1a16915fee49a31b5caf481211cb3f4637a
Emmanuel Kabari <kabariirenaeus@gmail.com>
2023-06-29 16:50:42 +0100

    added error logs

--- api/static/js/signup.js
+++ api/static/js/signup.js
@@ -30,7 +30,18 @@ signupForm.addEventListener("submit", async (event) => {
       console.log("Registration Error:", response.data.message);
     }
   } catch (error) {
-    console.log("Request Error:", error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      console.log("Response Error:", error.response.data);
+      console.log("Status Code:", error.response.status);
+      console.log("Status Text:", error.response.statusText);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log("Request Error:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an error
+      console.log("Error:", error.message);
+    }
   }
 });
 
@@ -68,10 +79,6 @@ signupForm.addEventListener("submit", async (event) => {
 //     console.log("Request Error:", error);
 //   }
 // });
-
-
-
-
 
 // // Function to handle the "Link History" button click event
 // function handleLinkHistoryButtonClick() {
