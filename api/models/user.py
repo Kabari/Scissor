cc56c991a1275fcc2bf1d5533e926c6672d1571a
Emmanuel Kabari <kabariirenaeus@gmail.com>
2023-06-29 15:43:12 +0100

    changed password_hash type to text

--- api/models/user.py
+++ api/models/user.py
@@ -10,7 +10,7 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password_hash = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     is_verified = db.Column(db.Boolean, nullable=False, default=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
